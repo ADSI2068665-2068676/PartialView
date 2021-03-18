fa3433652685d2ca9b09eb91b6dbc4736332597c
carlosfluna <carlosfluna@misena.edu.co>
2021-03-18 08:08:08 -0500

    homecontroller

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Vista2()
+        {
+            return View();
+        }
+        public ActionResult Vista3()
+        {
+            return View();
+        }
+    }
+}
